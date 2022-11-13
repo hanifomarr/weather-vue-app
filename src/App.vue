79dc4f0ea23f0587c200f646e881a8ecfb116024
hanifomarr <57039713+hanifomarr@users.noreply.github.com>
2022-11-13 19:15:54 +0800

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="flex flex-col "></div>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
