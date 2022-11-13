a5b9f7e66e87355fdd6d056178953d09a7452398
hanifomarr <57039713+hanifomarr@users.noreply.github.com>
2022-11-14 04:09:35 +0800

    update  API error handling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,15 @@
     <div class="pt-4 mb-8 relative">
       <input v-model="searchQuery" @input="getSearchResults" type="text" placeholder="Search by city/state"
         class="p-2 w-full bg-transparent border-b focus:border-color-secondary focus:outline-none focus:shadow-sm">
-        <ul v-if="mapboxSearchResults" class="absolute bg-color-secondary w-full shadow-md p-2 top-[66px] rounded-lg">
+      <ul v-if="mapboxSearchResults" class="absolute bg-color-secondary w-full shadow-md p-2 top-[66px] rounded-lg">
+        <p v-if="searchError">Sorry, something quiet wrong, please try again</p>
+        <p v-if="!searchError && mapboxSearchResults == 0">No result found, please try others location</p>
+        <template v-else>
           <li v-for="searchResult in mapboxSearchResults" :key="searchResult['id']" class="py-1 cursor-pointer">
-            {{ searchResult['place_name']}}
+            {{ searchResult['place_name'] }}
           </li>
-        </ul>
+        </template>
+      </ul>
     </div>
   </main>
 </template>
@@ -20,14 +24,19 @@ const searchQuery = ref("")
 const queryTimeout = ref()
 const mapboxAPIkey = "pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q"
 const mapboxSearchResults = ref(null)
+const searchError = ref()
 
 const getSearchResults = () => {
   clearTimeout(queryTimeout.value)
   queryTimeout.value = setTimeout(async () => {
     if (searchQuery.value !== "") {
-      const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIkey}&type=place`)
-      mapboxSearchResults.value = result.data.features
-      console.log(mapboxSearchResults.value)
+      try {
+        const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIkey}&type=place`)
+        mapboxSearchResults.value = result.data.features
+        console.log(mapboxSearchResults.value)
+      } catch{
+        searchError.value = true
+      }
       return
     } mapboxSearchResults.value = null
   }, 300);
