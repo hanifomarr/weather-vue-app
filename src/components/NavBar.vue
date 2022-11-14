5c6af6521fd33d74aca64fdde6554d79c8c9934c
hanifomarr <57039713+hanifomarr@users.noreply.github.com>
2022-11-15 02:11:58 +0800

    City view details page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
     <header class="sticky top-0 bg-color-primary shadow-lg">
-        <nav class="container flex flex-col sm:flex-row items-center gap-4 text-color-secondary py-6">
+        <nav class="container flex flex-row items-center gap-4 text-color-secondary py-6">
             <RouterLink :to="{ name: 'home' }">
                 <div class="flex items-center gap-3">
                     <i class="fa-solid fa-sun text-2xl"></i>
-                    <p class="text-2xl">Weather Application</p>
+                    <p class="text-2xl text-white">Weather Application</p>
                 </div>
             </RouterLink>
             <div class="flex gap-3 flex-1 justify-end">
